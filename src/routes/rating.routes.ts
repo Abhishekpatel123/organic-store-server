2938faf4977c40c20b84a21fb34134a56368454b
Abhishek <abhipatel8719@gmail.com>
2023-02-09 12:52:42 +0530

    fetch rating authenticate check remove

--- src/routes/rating.routes.ts
+++ src/routes/rating.routes.ts
@@ -1,26 +1,20 @@
-import { Router } from "express";
-import { authenticate, tryCatch } from "../middleware";
+import { Router } from 'express';
+import { authenticate, tryCatch } from '../middleware';
 
-import * as controllers from "../controllers/rating.controller";
-import * as validations from "../validations/rating.validation";
+import * as controllers from '../controllers/rating.controller';
+import * as validations from '../validations/rating.validation';
 
 const router = Router();
 
 // - Do rating
 router.post(
-  "/",
+  '/',
   authenticate,
   validations.doRating,
   tryCatch(controllers.doRating)
 );
 
 // - fetch rating and review
-router.get(
-  "/:productId",
-  authenticate,
-  tryCatch(controllers.fetchRatingAndReview)
-);
-
-
+router.get('/:productId', tryCatch(controllers.fetchRatingAndReview));
 
 export default router;
